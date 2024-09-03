9dcd800b7b1bd0f66c653ce7063349fc2753633a
Guilherme Freitas <139295562+GuilhermeSK2@users.noreply.github.com>
2024-09-03 16:38:26 -0300

    First Commit

---
+++ Estrutura de dados/1 - Listas/declarandoLista.py
@@ -0,0 +1,18 @@
+"""Exemplo de declaração de listas em Python"""
+
+frutas = ["laranja", "maca", "uva"]
+
+#frutas = []
+
+#letras = list("python")
+
+#numeros = list(range(10))
+
+#carro = ["Ferrari", "F8", 4200000, 2020, 2090, "São Paulo", True]
+
+
+frutas = [0] #laranja
+frutas = [2] #uva
+
+frutas = [-1] #uva
+frutas = [-2] #maca
